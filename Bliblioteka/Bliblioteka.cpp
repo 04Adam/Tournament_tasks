74d9065fb791db9eb378884943232580cdab64d7
04Adam <100508051+04Adam@users.noreply.github.com>
2022-02-27 08:45:06 +0100

    Add files via upload

---
+++ Bliblioteka/Bliblioteka.cpp
@@ -0,0 +1,48 @@
+﻿// Bliblioteka.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int>K;
+    char N;
+    int t;
+    int l;
+
+
+
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        cin >> N;
+        cin >> l;
+        if (N == 'L') {
+            K.insert(K.begin(), l);
+        }
+        else if (N == 'R') {
+            K.push_back(l);
+        }
+        else {
+            auto itr = find(K.begin(), K.end(), l);
+            int s = K.size();
+            if (itr != end(K))
+            {
+                int x = distance(K.begin(), itr);
+                
+                if (x >= s / 2) {
+                    cout << s - x - 1<<'\n';
+                }
+                else { cout << x << '\n'; }
+            }
+            
+            }
+
+        
+    }
+
+    return 0;
+}
+
+
