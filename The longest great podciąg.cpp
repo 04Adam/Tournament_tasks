b63c1856aaf6b86f11d942662721ad46047f45f1
04Adam <100508051+04Adam@users.noreply.github.com>
2022-03-26 09:55:53 +0100

    Add files via upload

---
+++ The longest great podciąg.cpp
@@ -0,0 +1,42 @@
+﻿// The longest great podciąg.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    map<char, int>M{};
+    
+    string a="";
+    int n, k;
+    cin >> n;
+    cin >> k;
+    cin >> a;
+    vector<int>V;
+    for (int i = 0; i < n; i++) {
+        M[a[i]] += 1;
+     
+    }
+    int licz = 1;
+    for (auto m:M) {
+        V.push_back(m.second);
+        if (licz == k)
+            break;
+        licz++;
+    }
+    if (V.size() < k) {
+        cout << "0";
+        
+    }
+    else {
+        sort(V.begin(), V.end());
+        cout << V[0] * k;
+    }
+        return 0;
+    
+
+}
+
---
+++ login for evety one.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+    string a;
+    string b;
+    cin >> a;
+    cin >> b;
+    int i = 1;
+    cout << a[0];
+    while ((int)a[i] < (int)b[0]) {
+        if (i >= a.length()) {
+            break;
+        }
+               
+        cout << a[i];
+        i++;
+        
+    }
+
+
+    cout << b[0];
+    return 0;
+}
+
